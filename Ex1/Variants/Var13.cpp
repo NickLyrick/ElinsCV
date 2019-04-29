733b6f068ce4f70b54178add528808f943ec64cf
yaskovMC <yaskov.public@gmail.com>
2019-04-29 19:54:25 +0300

    Ex1: added Var13.cpp | 2

--- Ex1/Variants/Var13.cpp
+++ Ex1/Variants/Var13.cpp
@@ -10,5 +10,26 @@ using namespace std;
 
 void var13()
 {
-    //Здесь должен быть ваш код
+    Mat img = imread("Lenna.png");
+
+    if (!img.data)
+        printf("No image data \n");
+    else {
+	    // Добавляем текст
+	    putText(img,"yaskovMC",Point(img.cols/4,img.rows/4*3),FONT_HERSHEY_PLAIN,3,Scalar(0,255,0),2);
+
+	    // Добавляем три крестика
+	    drawMarker(img,Point(img.cols/2-40,img.rows/2),Scalar(255,0,0),MARKER_CROSS,40,2);
+	    drawMarker(img,Point(img.cols/2,img.rows/2),Scalar(0,255,0),MARKER_CROSS,40,2);
+	    drawMarker(img,Point(img.cols/2+40,img.rows/2),Scalar(0,0,255),MARKER_CROSS,40,2);
+
+	    // отображаем в окне
+		namedWindow("Display");
+		imshow("Display",img);
+
+		// сохранение
+		imwrite("Var13.png",img);
+
+		waitKey(0);	
+    }
 } 
