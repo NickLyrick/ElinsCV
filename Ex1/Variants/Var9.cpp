048bba8acdc7693676b19b3f202b0eeb18e43f67
Kopytin Artem <slipmetallovec@gmail.com>
2019-05-24 11:38:37 +0300

    Font correction for 1 and 2st lessons
    1- three function for HW_1_CV
    
    2 step - one function putText()
    
    2 step - write my name on English, because appeare error
    
    4 step - watch a exemple
    
    5 step
    
    6 step - IT WORKS!!!

--- Ex1/Variants/Var9.cpp
+++ Ex1/Variants/Var9.cpp
@@ -10,5 +10,28 @@ using namespace std;
 
 void var9()
 {
-    //Здесь должен быть ваш код
+    
+    //Mat img = imread("Lenna.png");
+    //putText
+        //( img,   "Pleshko Lizaveta" ,  Point(img.cols/4,img.rows/4*3), FONT_HERSHEY_SIMPLEX, 3,Scalar(0,255,0),2);
+          
+          //fontScale, blue, thickness = 3, lineType = LINE_8, bottomLeftOrigin = false
+        
+    //namedWindow("Display");
+    //imshow("Display",img);
+    
+    Mat img = imread("Lenna.png");
+
+	    // Добавляем текст
+	    putText(img,"yaskovMC",Point(img.cols/4,img.rows/4*3),FONT_HERSHEY_PLAIN,3,Scalar(0,255,0),2);
+
+	   
+
+	    // отображаем в окне
+		namedWindow("Display", CV_WINDOW_AUTOSIZE);
+		imshow("Display",img);
+
+
+		waitKey(0);	
+    
 } 
